70bdbe1ffdbcd8abfb84fd63930e7f12b7ebe5a7
daisuke.somei <d.somei@leafnet.jp>
2024-06-19 06:24:28 +0000

    timeline

--- app/Models/User.php
+++ app/Models/User.php
@@ -99,4 +99,14 @@ class User extends Authenticatable
     {
         return $this->followings()->where('follow_id', $userId)->exists();
     }
+    
+    //このユーザーとフォロー中のユーザーの投稿に絞り込む
+    public function feed_microposts(){
+        //このユーザーがフォロー中のユーザーidを取得して配列にする
+        $userIds = $this->followings()->pluck('users.id')->toArray();
+        //このユーザーのidもその配列に追加
+        $userIds[] = $this->id;
+        //それらのユーザーが所有する投稿に絞り込む
+        return Micropost::whereIn('user_id', $userIds);
+    }
 }
